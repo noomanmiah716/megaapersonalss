a5b824cc3b682118e308bc43f595687ee386af22
Zayn Yusuf <yusue786@gmail.com>
2025-03-05 14:21:59 +0600

    first commit

---
+++ app/connecting/page.jsx
@@ -0,0 +1,9 @@
+export default function ConnectingPage() {
+  return (
+    <div className="relative text-black h-screen w-screen flex flex-col justify-center items-center">
+      <div className="absolute top-[40px] lg:top-[140px] text-black">
+        <h1 className="text-center font-bold text-3xl">Connecting...</h1>
+      </div>
+    </div>
+  );
+}
